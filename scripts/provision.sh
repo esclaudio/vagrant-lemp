a5a71381e44eeedaa743eab2e7263f0e5114704b
Claudio Escobar <claudioe0407@gmail.com>
2021-09-07 11:09:02 -0300

    Add puppeteer

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -41,14 +41,17 @@ if [ ! -f /home/vagrant/.provisioned/.php7 ] ; then
     touch /home/vagrant/.provisioned/.php7
 fi
 
-# # Python 3 (pip)
+# Puppeteer
+if [ ! -f /home/vagrant/.provisioned/.puppeteer ] ; then
+    echo "Installig Puppeteer"
 
-# if [ ! -f /home/vagrant/.provisioned/.python3 ] ; then
-#     echo "Installig PYTHON"
+    curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+    sudo apt-get install -y nodejs gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget libgbm-dev libxshmfence-dev
+    sudo npm install --global --unsafe-perm puppeteer
+    sudo chmod -R o+rx /usr/lib/node_modules/puppeteer/.local-chromium
 
-#     DEBIAN_FRONTEND=noninteractive apt-get install -yq python3-pip
-#     touch /home/vagrant/.provisioned/.python3
-# fi
+    touch /home/vagrant/.provisioned/.puppeteer
+fi
 
 # WKHtml
 
@@ -60,7 +63,7 @@ if [ ! -f /home/vagrant/.provisioned/.wkhtml ] ; then
     wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
     sudo dpkg -i wkhtmltox_0.12.5-1.bionic_amd64.deb
     sudo ln -s /usr/local/bin/wkhtmltopdf /usr/bin
-	sudo ln -s /usr/local/bin/wkhtmltoimage /usr/bin
+    sudo ln -s /usr/local/bin/wkhtmltoimage /usr/bin
 
     touch /home/vagrant/.provisioned/.wkhtml
 fi
